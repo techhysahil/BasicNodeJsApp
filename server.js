9471036933e87cfc220ceea85838513c6dba964f
Sahil <sahil@unicommerce.com>
2015-06-30 04:55:12 +0530

    cjg

--- server.js
+++ server.js
@@ -1,34 +1,15 @@
-var cc          = require('config-multipaas'),
-    restify     = require('restify'),
-    fs          = require('fs'),
-    path        = require('path'),
-    db          = require(path.join(__dirname,'bin','db.js'))
+var express = require('express');
+var app = express();
 
-var config      = cc()
-var app         = restify.createServer()
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
 
-app.use(restify.queryParser())
-app.use(restify.CORS())
-app.use(restify.fullResponse())
+var server = app.listen(8081, function () {
 
-// Routes
-app.get('/parks/within', db.selectBox);
-app.get('/parks', db.selectAll);
-app.get('/status', function (req, res, next)
-{
-  res.send("{status: 'ok'}");
-});
+    var host = server.address().address
+    var port = server.address().port
 
-app.get('/', function (req, res, next)
-{
-  var data = fs.readFileSync(path.join(__dirname, 'index.html'));
-  res.status(200);
-  res.header('Content-Type', 'text/html');
-  res.end(data.toString().replace(/host:port/g, req.header('Host')));
-});
+    console.log("Example app listening at http://%s:%s", host, port)
 
-app.get(/\/(css|js|img)\/?.*/, restify.serveStatic({directory: path.join(__dirname,'static')}));
-
-app.listen(config.get('PORT'), config.get('IP'), function () {
-  console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )
-});
+})
